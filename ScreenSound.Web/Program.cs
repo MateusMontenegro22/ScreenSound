585cb76b0fd9a172a9f4afb30cc5bcfcd01fcd1e
unknown <mateusmontenegro65@gmail.com>
2024-07-17 22:14:53 -0300

    instalando o mudblazor na aplicação

--- ScreenSound.Web/Program.cs
+++ ScreenSound.Web/Program.cs
@@ -1,17 +1,28 @@
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Components.RenderTree;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using MudBlazor.Services;
 using ScreenSound.Web;
 using ScreenSound.Web.Services;
+using System.Reflection.Emit;
+using System.Reflection.Metadata;
+using System.Runtime.CompilerServices;
+using System.Security.Cryptography.X509Certificates;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
-
+builder.Services.AddTransient<MusicaAPI>();
 builder.Services.AddTransient<ArtistaAPI>();
+builder.Services.AddMudServices();
+
 
 builder.Services.AddHttpClient("API", client => {
     client.BaseAddress = new Uri(builder.Configuration["APIServer:Url"]!);
     client.DefaultRequestHeaders.Add("Accept", "application/json");
 });
+
+
 
 await builder.Build().RunAsync();
---
+++ ScreenSound.Web/Services/MusicaAPI.cs
@@ -0,0 +1,19 @@
+﻿using MudBlazor;
+using ScreenSound.Web.Response;
+using System.Net.Http.Json;
+using System.Runtime.CompilerServices;
+
+namespace ScreenSound.Web.Services;
+public class MusicaAPI
+{
+	private readonly HttpClient _httpClient;
+
+	public MusicaAPI(HttpClient httpClient)
+	{
+		_httpClient = httpClient;
+	}
+	public async Task<ICollection<MusicaResponse>?> GetArtistasAsync()
+	{
+		return await _httpClient.GetFromJsonAsync<ICollection<MusicaResponse>>("musicas");
+	}
+}
